65a9747b6f7b5c9279e6e9deba0aa6751ce13694
sebvstiansoto <sebastian.soto.c06@gmail.com>
2024-08-26 19:46:26 -0400

    Solucion navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,65 +1,149 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faXTwitter,
-  faGithub,
-  faLinkedin,
-  faUser,
-  faShoppingCart,
-} from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from 'react-router-dom'; 
+import React, { useState } from 'react';
+import profileUser from './images/profile-user.png';
+import chatIcon from './images/chat.png';
+import tagIcon from './images/tag.png';
+import bellIcon from './images/bell.png';
 
-const logout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('username');
-    navigate('/login'); 
-};
-
-const username = localStorage.getItem('username');
-const navigate = useNavigate(); 
-
-const NavBar = () => {
-  const isLoggedIn = Boolean(localStorage.getItem("userEmail"));
-
-  const renderIconLink = (href, icon) => (
-    <a href={href} className="hover:text-black">
-      <FontAwesomeIcon icon={icon} />
-    </a>
-  );
+const Navbar = () => {
+  const [selectedRadio, setSelectedRadio] = useState('btnradio1');
 
   return (
-    <nav className="flex justify-between items-center text-white h-16 bg-gray-600">
-      <div className="flex items-center space-x-4 ml-8">
-        {renderIconLink("https://twitter.com", faXTwitter)}
-        {renderIconLink("https://github.com", faGithub)}
-        {renderIconLink("https://linkedin.com", faLinkedin)}
-      </div>
-      <div className="flex flex-col items-center">
-        <a href="/" className="flex items-center flex-col">
-          <img
-            src="../assets/light-icon.png"
-            alt="Project Icon"
-            className="w-auto rounded-full object-cover h-12 mr-2 px-2"
+    <nav className="navbar bg-body-tertiary fixed-top">
+      <div className="container-fluid">
+        <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
+          <input
+            type="radio"
+            className="btn-check"
+            name="btnradio"
+            id="btnradio1"
+            autoComplete="off"
+            checked={selectedRadio === 'btnradio1'}
+            onChange={() => setSelectedRadio('btnradio1')}
           />
-        </a>
-      </div>
-      <div className="flex items-center space-x-2 mr-8">
-        <a
-          href={isLoggedIn ? "/account" : "/login"}
-          className="hover:text-black"
+          <label className="btn btn-outline-primary btn btn-outline-success" htmlFor="btnradio1">
+            <img width="20px" height="20px" src={profileUser} alt="Profile" />
+          </label>
+
+          <input
+            type="radio"
+            className="btn-check"
+            name="btnradio"
+            id="btnradio2"
+            autoComplete="off"
+            checked={selectedRadio === 'btnradio2'}
+            onChange={() => setSelectedRadio('btnradio2')}
+          />
+          <label className="btn btn-outline-primary btn btn-outline-success" htmlFor="btnradio2">
+            <img width="20px" height="20px" src={chatIcon} alt="Chat" />
+          </label>
+
+          <input
+            type="radio"
+            className="btn-check"
+            name="btnradio"
+            id="btnradio3"
+            autoComplete="off"
+            checked={selectedRadio === 'btnradio3'}
+            onChange={() => setSelectedRadio('btnradio3')}
+          />
+          <label className="btn btn-outline-primary btn btn-outline-success" htmlFor="btnradio3">
+            <img width="20px" height="20px" src={tagIcon} alt="Tag" />
+          </label>
+
+          <input
+            type="radio"
+            className="btn-check"
+            name="btnradio"
+            id="btnradio4"
+            autoComplete="off"
+            checked={selectedRadio === 'btnradio4'}
+            onChange={() => setSelectedRadio('btnradio4')}
+          />
+          <label className="btn btn-outline-primary btn btn-outline-success" htmlFor="btnradio4">
+            <img width="20px" height="20px" src={bellIcon} alt="Notifications" />
+          </label>
+        </div>
+
+        <div className="d-flex flex-column">
+          <a className="btn btn-warning btn-outline-dark" href="#">
+            Publicar
+          </a>
+        </div>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+          aria-label="Toggle navigation"
         >
-          <FontAwesomeIcon icon={faUser} />
-          <span>{isLoggedIn ? "ACCOUNT" : "LOGIN"}</span>
-        </a>
-        <a href="/cart" className="hover:text-black">
-          <div className="flex items-center">
-            <FontAwesomeIcon icon={faShoppingCart} />
-            <span className="ml-1">{totalItems}</span>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="offcanvas offcanvas-end"
+          tabIndex="-1"
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+        >
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+              ComunidApp
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
           </div>
-        </a>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Noticias
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Publicaciones
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Salud
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Eventos
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Servicios
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
 };
 
-export default NavBar;
+export default Navbar;
+
